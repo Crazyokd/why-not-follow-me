717c857cd10fe5ea4fc9e8142a9f76cb1cd19f4a
Rekord <1324596506@qq.com>
2022-01-13 00:35:02 +0800

    update push.bat - add function of push tags

--- push.bat
+++ push.bat
@@ -1,9 +1,16 @@
 @echo off
-:push 
+:push_main
 git push origin main
 
 if errorlevel 1 (
-    goto :push
+    goto :push_main
+)
+
+:push_tag
+git push origin --tags
+
+if errorlevel 1 (
+    goto :push_tag
 ) else (
     echo Push Succeeded
 )
