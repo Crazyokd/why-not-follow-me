faa890166f3c248536cde654f2fb3c98e94fbe9e
Rekord <1324596506@qq.com>
2022-01-13 00:23:00 +0800

    支持分页抓取

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ following_usernames = []
 followers_nicknames = []
 following_nicknames = []
 
-def visit(tab:str):
-    url = f"https://github.com/Crazyokd?tab={tab}"
+def visit(tab:str, page:int=1):
+    url = f"https://github.com/Crazyokd?page={page}&tab={tab}"
     response = requests.get(url=url,timeout=10,headers=headers,proxies=proxies)
     if response.status_code == 200:
         print(f"Successful visit {url}")
@@ -37,15 +37,18 @@ def visit(tab:str):
     if text == None:
         raise Exception("match string error! please check the match string")
     # write to file
-    with open(f'{tab}.txt','w',encoding='utf-8') as f:
+    with open(f'{tab}_{page}.txt','w',encoding='utf-8') as f:
         f.write(text)
 
 
-def handle_data(tab:str):
-    with open(f'{tab}.txt','r',encoding='utf-8') as f:
+def handle_data(tab:str, page:int=1):
+    with open(f'{tab}_{page}.txt','r',encoding='utf-8') as f:
         text = f.read()
     re_nickname = re.compile('<span class="f4 Link--primary(.*?)/.*?>',re.S)
     re_username = re.compile('<span class="Link--secondary.*?">(.*?)</span>',re.S)
+    re_page = re.compile('class="pagination">.*?Previous.*?page=(\d+).*?Next',re.S)
+
+    page = re.findall(re_page,text)
     nicknames = re.findall(re_nickname,text)
     # print(nicknames)    # print all nicknames
     usernames = re.findall(re_username,text)
@@ -60,16 +63,19 @@ def handle_data(tab:str):
         global followers_num
         global followers_nicknames
         global followers_usernames
-        followers_num = num
-        followers_nicknames = nicknames
-        followers_usernames = usernames
+        followers_num += num
+        followers_nicknames.extend(nicknames)
+        followers_usernames.extend(usernames)
     else:
         global following_num
         global following_nicknames
         global following_usernames
-        following_num = num
-        following_nicknames = nicknames
-        following_usernames = usernames
+        following_num += num
+        following_nicknames.extend(nicknames)
+        following_usernames.extend(usernames)
+    
+    if len(page) != 0:
+        get_data(tab,int(page[0]))
 
 
 def print_separator():
@@ -78,12 +84,12 @@ def print_separator():
     print()
 
 
-def get_data(tab:str):
+def get_data(tab:str, page:int=1):
     success = False
     while not success:
         try:
-            # visit(tab)
-            handle_data(tab)
+            visit(tab, page)
+            handle_data(tab, page)
             success = True
         except requests.exceptions.ReadTimeout:
             error_msg = 'ReadTimeout!'
@@ -108,30 +114,40 @@ def get_result():
     # You follow to the other person and who doesn't follow you
     print()
     print("You follow to the other person and who doesn't follow you:\n")
-    i = 0
+    count = 0
     for i in range(following_num):
         try:
-            index = followers_nicknames.index(following_nicknames[i])
+            index = followers_usernames.index(following_usernames[i])
         except ValueError:
             print(f"[{following_nicknames[i]}](https://github.com/{following_usernames[i]})")
+            count += 1
         else:
             followers_nicknames.pop(index)
             followers_usernames.pop(index)
-        
-
+    
+    print(f"\ntotal:{count}")
     print_separator()
 
     # Anothor person follow you and you doesn't follow him/her.
     print("Anothor person follow you and you doesn't follow him/her:\n")
     for i in range(len(followers_nicknames)):
         print(f"[{followers_nicknames[i]}](https://github.com/{followers_usernames[i]})")
+    
+    print(f"\ntotal:{len(followers_nicknames)}")
+
+
+def save_file():
+    with open('followers.txt','w',encoding='utf-8') as f:
+        for i in range(len(followers_nicknames)):
+            f.write(f"[{followers_nicknames[i]}](")
+
 
 def start():
     get_data("followers")
     # print separator
     print_separator()
     get_data("following")
-
+    # get result
     get_result()
 
 if __name__=='__main__':
